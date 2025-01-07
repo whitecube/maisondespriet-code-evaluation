69e0aa3ef6417fbd9bca32adda1a85e590c30e7c
Toon Van den Bos <toon@whitecube.be>
2025-01-07 09:46:14 +0100

    Added missing SoftDeletes trait

--- app/Models/Supplier.php
+++ app/Models/Supplier.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Supplier extends Model
 {
+    use SoftDeletes;
+    
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
