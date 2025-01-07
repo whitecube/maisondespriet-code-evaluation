5589308c5f1eb0a22212b8bfb27d171fcc088b7d
Toon Van den Bos <toon@whitecube.be>
2025-01-07 11:43:12 +0100

    Added missing client orders relationship

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -6,6 +6,7 @@ use App\Transactions\Clients\ClientType;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Client extends Model
 {
@@ -26,5 +27,10 @@ class Client extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function orders(): HasMany
+    {
+        return $this->hasMany(Order::class);
     }
 }
