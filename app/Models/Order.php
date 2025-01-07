e27cf4d5fc1a1e001c0a27f270de4e1e56751dbc
Toon Van den Bos <toon@whitecube.be>
2025-01-07 11:27:19 +0100

    Added orders & order_products migrations & models

---
+++ app/Models/Order.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Models;
+
+use App\Transactions\Orders\OrderStatus;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Order extends Model
+{
+    use SoftDeletes;
+    
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'status' => OrderStatus::class,
+        ];
+    }
+
+    public function client(): BelongsTo
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function products(): HasMany
+    {
+        return $this->hasMany(OrderProduct::class);
+    }
+}
---
+++ app/Models/OrderProduct.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class OrderProduct extends Model
+{
+    public function order(): BelongsTo
+    {
+        return $this->belongsTo(Order::class);
+    }
+}
